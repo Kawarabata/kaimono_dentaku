d674b90a973caf5e414c0211b1b2d232a7c3cdf3
Kawarabata <kwrbt0404@gmail.com>
2021-01-17 18:40:43 +0900

    Create FormulaListItem component and add animation

---
+++ components/FormulaListItem.vue
@@ -0,0 +1,102 @@
+<template>
+  <li :class="['formula-list-item', { 'min-result': isMinResult }]">
+    <img
+      v-if="isMinResult"
+      src="@/assets/images/flag_icon.svg"
+      alt="最安"
+      class="flag-icon"
+    />
+    <div class="formula-container">
+      <span class="bold">{{ price }}</span>
+      <span class="unit">円</span>
+      <span class="operator">÷</span>
+      <span class="bold">{{ gram }}</span>
+      <span class="unit">g</span>
+      <span class="operator">=</span>
+      <span class="bold">{{ result }}</span>
+      <span class="unit">円</span>
+    </div>
+    <button class="garbage-icon" @click="removeFormula">
+      <img src="@/assets/images/garbage_white_icon.svg" alt="削除" />
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    price: {
+      type: String,
+      required: true,
+    },
+    gram: {
+      type: String,
+      required: true,
+    },
+    result: {
+      type: Number,
+      required: true,
+    },
+    isMinResult: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    removeFormula() {
+      this.$emit('remove-formula')
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.formula-list-item {
+  display: flex;
+  padding: 16px 16px 16px 47px;
+  background-color: #e0f1f1;
+  border-radius: 8px;
+
+  &.min-result {
+    padding: 16px;
+  }
+
+  & + li {
+    margin-top: 16px;
+  }
+
+  & .flag-icon {
+    margin-right: 16px;
+  }
+
+  & .formula-container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-right: 24px;
+    width: 100%;
+
+    & span {
+      display: block;
+    }
+
+    & .bold {
+      font-weight: bold;
+    }
+
+    & .unit {
+      font-size: 14px;
+    }
+
+    & .operator {
+      font-size: 24px;
+      margin-top: -6px;
+    }
+  }
+
+  & .garbage-icon {
+    display: block;
+    margin-left: auto;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,31 +5,17 @@
       <button @click="resetList">All Reset</button>
     </header>
     <ul>
-      <li
-        v-for="(formula, index) in formulaList"
-        :key="index"
-        :class="{ 'min-result': isMinResult(index) }"
-      >
-        <img
-          v-if="index === minResultIndex"
-          src="@/assets/images/flag_icon.svg"
-          alt="最安"
-          class="flag-icon"
+      <transition-group name="formulae">
+        <formula-list-item
+          v-for="(formula, index) in formulaList"
+          :key="formula.id"
+          :price="formula.price"
+          :gram="formula.gram"
+          :result="formula.result"
+          :is-min-result="isMinResult(index)"
+          @remove-formula="removeFormula(index)"
         />
-        <div class="formula-container">
-          <span class="bold">{{ formula.price }}</span>
-          <span class="unit">円</span>
-          <span class="operator">÷</span>
-          <span class="bold">{{ formula.gram }}</span>
-          <span class="unit">g</span>
-          <span class="operator">=</span>
-          <span class="bold">{{ formula.result }}</span>
-          <span class="unit">円</span>
-        </div>
-        <button class="garbage-icon" @click="removeFormula(index)">
-          <img src="@/assets/images/garbage_white_icon.svg" alt="削除" />
-        </button>
-      </li>
+      </transition-group>
     </ul>
     <footer>
       <div class="footer-container">
@@ -110,11 +96,14 @@ export default {
   },
   methods: {
     addFormula() {
-      if (!this.price || !this.gram) {
+      if (this.isFormulaInvalid) {
         return
       }
 
+      const id =
+        this.formulaList.length > 0 ? this.formulaList.slice(-1)[0].id + 1 : 1
       this.$store.dispatch('addFormula', {
+        id,
         price: this.price,
         gram: this.gram,
         result: this.result,
@@ -159,55 +148,10 @@ export default {
     padding: 24px 12px;
     max-width: 380px;
     margin: 0 auto;
+  }
 
-    & li {
-      display: flex;
-      padding: 16px 16px 16px 47px;
-      background-color: #e0f1f1;
-      border-radius: 8px;
-
-      &.min-result {
-        padding: 16px;
-      }
-
-      & + li {
-        margin-top: 16px;
-      }
-
-      & .flag-icon {
-        margin-right: 16px;
-      }
-
-      & .formula-container {
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        margin-right: 24px;
-        width: 100%;
-
-        & span {
-          display: block;
-        }
-
-        & .bold {
-          font-weight: bold;
-        }
-
-        & .unit {
-          font-size: 14px;
-        }
-
-        & .operator {
-          font-size: 24px;
-          margin-top: -6px;
-        }
-      }
-
-      & .garbage-icon {
-        display: block;
-        margin-left: auto;
-      }
-    }
+  & .formula-list-item {
+    transition: all 0.6s;
   }
 
   & footer {
@@ -268,5 +212,19 @@ export default {
       }
     }
   }
+}
+
+.formulae-enter {
+  opacity: 0;
+  transform: translateY(350px);
+}
+
+.formulae-leave-to {
+  opacity: 0;
+  transform: translateX(350px);
+}
+
+.formulae-leave-active {
+  position: absolute;
 }
 </style>
